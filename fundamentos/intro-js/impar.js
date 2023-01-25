f0d75fac382e4f8562d3e10b7ce27d2d995b31fb
Rodney Nunes <rodynunes@gmail.com>
2023-01-25 14:48:58 -0300

    mais exercicios do primeiro dia de js

---
+++ fundamentos/intro-js/impar.js
@@ -0,0 +1,11 @@
+const a = 2;
+const b = 6;
+const c = 4;
+
+let impar = false;
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !==0)) {
+    impar = true;
+};
+
+console.log (impar);
---
+++ fundamentos/intro-js/par.js
@@ -0,0 +1,11 @@
+const a = 3;
+const b = 5;
+const c = 9;
+
+let outros = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 ===0)) {
+    outros = true;
+};
+
+console.log (outros);
